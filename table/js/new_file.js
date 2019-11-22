ec2fc0c058f735733413cf416e4121a032b0b5be
QuarkL <745020259@qq.com>
2019-11-22 10:24:27 +0800

    Initial commit

---
+++ table/js/new_file.js
@@ -0,0 +1,22 @@
+var count=0;
+function add()
+{
+	
+	var a = document.createElement("tr");
+	var tab = document.getElementById("biao");
+	var name = document.getElementById("name").value;
+	var num = document.getElementById("num").value;
+	tab.innerHTML+=
+	"<tr id='"+count+"'>"+
+	"<td>"+name+"</td>"+
+	"<td>"+num+"</td>"+
+	'<td><a href="javascript:delet('+count+')">'+
+	"删除"+
+	"</a></td>"+
+	"</tr>";
+	count++;
+}
+function delet(count){
+    var row = document.getElementById(count);
+    row.remove(count);
+}
